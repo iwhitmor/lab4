3ed183ec01dbfae6d11d225bd4b0da532256c06e
Ian Whitmore <ianwhitmor@gmail.com>
2021-07-21 20:26:12 -0500

    add content to .js

---
+++ app.js
@@ -0,0 +1,11 @@
+var answer = prompt("Do you want a cut?")
+
+if (answer === "yes") {
+   alert("That's what I'm talking about!");
+}
+else if (answer === "no") {
+    alert("Aw shucks!")
+}
+else {
+    alert("Stay focused on your mission, remain steadfast in your pursuit of excellence, and come and get a sweet cut!")
+}
